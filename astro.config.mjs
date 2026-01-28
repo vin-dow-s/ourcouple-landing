c13e908a13e4fa3197d767c228442c21c2eb510d
Vincent S <esyfer@free.fr>
2026-01-28 14:47:18 +0100

    LP launch v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ourcouple.app',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
